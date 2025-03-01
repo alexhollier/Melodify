5c54f0684c78198ce6033e14bf915aa742f82834
va2324 <VasuK1234@outlook.com>
2025-03-01 13:44:28 -0600

    Created lesson pages

---
+++ MusicApp/app/lessons/2notation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+export default function Notation(){
+    return(
+        <ScrollView>
+            <Text>
+                Notation
+            </Text>
+        </ScrollView>
+    );
+}
