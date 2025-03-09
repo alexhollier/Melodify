33662fbfbe129217b49f0db013e02fcc67666d53
va2324 <VasuK1234@outlook.com>
2025-03-08 19:01:58 -0600

    whatever

--- MusicApp/app/lessons/0contents.tsx
+++ MusicApp/app/lessons/0contents.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Link} from 'expo-router';
-
+//whatever
 export default function Contents(){
     return(
         <View>
