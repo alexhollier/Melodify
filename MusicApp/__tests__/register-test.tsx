187a204264302b2d8aef08bbf4048a8bec0cc0a6
va2324 <VasuK1234@outlook.com>
2025-03-23 19:16:18 -0500

    mock fierbase/auth

--- MusicApp/__tests__/register-test.tsx
+++ MusicApp/__tests__/register-test.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import Register from '@/components/Register';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+
+jest.mock('firebase/auth', () => {
+    const auth = {
+        createUserWithEmailAndPassword: jest.fn(),
+        updateProfile: jest.fn()
+    };
+    return {
+        getAuth: jest.fn(() => auth),
+        ...auth
+    };
+});
 
 describe('register', () => {
     test('renders successfully', () => {
