42544e09f9c478f3b81677030571217ab56203cd
va2324 <VasuK1234@outlook.com>
2025-03-28 20:21:45 -0500

    tests

--- MusicApp/__tests__/home-test.tsx
+++ MusicApp/__tests__/home-test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { render } from '@testing-library/react-native';
 import HomeScreen from '@/app/(tabs)/home';
 
 describe('HomeScreen', () => {
     test('renders correctly', () => {
-      render(<NavigationContainer><HomeScreen /></NavigationContainer>);
+      render(<HomeScreen />);
     });
 });
