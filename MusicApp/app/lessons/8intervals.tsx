cf8ff3348a494fecdde1f6d061eb7a61b71b3571
va2324 <VasuK1234@outlook.com>
2025-04-07 15:19:09 -0500

    fixed links

--- MusicApp/app/lessons/8intervals.tsx
+++ MusicApp/app/lessons/8intervals.tsx
@@ -5,13 +5,16 @@ import {Link} from 'expo-router';
 export default function Intervals(){
     return(
         <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.text}>
+            <Text style={styles.title}>
                 Intervals
             </Text>
-            <Link href='./7modes'>
+            <Link href='./7modes' style={styles.edgelinks}>
                 Previous: Modes
             </Link>
-            <Link href='./9melody'>
+            <Link href='/(tabs)/home' style={styles.homelink}>
+                MusicApp
+            </Link>
+            <Link href='./9melody' style={styles.edgelinks}>
                 Next: Melody
             </Link>
         </ScrollView>
@@ -24,8 +27,44 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         alignItems: 'center'
     },
+    title: {
+        color: 'black',
+        fontSize: 80,
+        fontFamily: 'TIMES_NEW_ROMAN',
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        paddingBottom: 20
+    },
     text: {
         color: 'black',
-        fontSize: 14
+        fontSize: 20,
+        fontFamily: 'ARIAL',
+        padding: 10
+    },
+    header: {
+        color: 'black',
+        fontSize: 36,
+        fontFamily: 'ARIAL',
+        fontWeight: 'bold',
+        padding: 40,
+        paddingTop: 60,
+        textAlign: 'left',
+    },
+    buttons: {
+        flexDirection: 'row'
+    },
+    links: {
+        flexDirection: 'row',
+        padding: 40,
+        gap: 500
+    },
+    edgelinks: {
+        color: 'purple',
+        fontSize: 30
+    },
+    homelink: {
+        color: 'purple',
+        fontSize: 30,
+        alignSelf: 'center'
     }
 })
