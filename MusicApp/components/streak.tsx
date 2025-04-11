07a803e7f10e6344071c7169f1697aedcecb81f1
BellaFred <156227097+BellaFred@users.noreply.github.com>
2025-04-11 13:32:09 -0500

    implemented logic so that streak updates as expected upon login

--- MusicApp/components/streak.tsx
+++ MusicApp/components/streak.tsx
@@ -1,13 +1,46 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 import { Link, Stack } from 'expo-router';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { auth, db } from '@/firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import {useAuth} from '../app/context/AuthContext';
 
-type Props = {
-  label: string;
-  streak: number;
-};
+const Streak: React.FC=() =>{
+  const {user}=useAuth();
+  const [streak, setStreak]= useState('');
+  const [userId, setUserId]= useState('');
 
-export default function Streak({ label, streak }: Props) {
+  useEffect(()=>{
+    if (auth.currentUser){
+      setUserId(auth.currentUser.uid);
+    }
+  }, []);
+
+  useEffect(()=>{
+    const fetchUserData= async()=>{
+      if(userId){
+        console.log('Fetching data for userId:', userId);
+
+        try{
+          const userRef= doc(db, 'users', userId);
+          const userDoc = await getDoc(userRef)
+          
+          if (userDoc.exists()) {
+            console.log('Document data:', userDoc.data());
+            setStreak(userDoc.data()?.streak || '');
+          } else {
+            console.log('No such document!');
+          }
+  
+        }catch(error){
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+    fetchUserData();
+  }, [userId]);
+  
   return (
     <View style={styles.streakContainer}>
         <Link href="/challenges" style={styles.streak}>
@@ -28,3 +61,4 @@ const styles = StyleSheet.create({
     right: 10,
   }
 });
+export default Streak;
