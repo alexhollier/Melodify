d19a0a5e3007e10494bc30bac9a766172f39cdec
va2324 <VasuK1234@outlook.com>
2025-04-13 17:54:53 -0500

    began melody lesson

--- MusicApp/app/lessons/9melody.tsx
+++ MusicApp/app/lessons/9melody.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
-import {Text, ScrollView, StyleSheet} from 'react-native';
+import {Text, ScrollView, StyleSheet, View} from 'react-native';
 import {Link} from 'expo-router';
 
 export default function Melody(){
     return(
         <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.text}>
+            <Text style={styles.title}>
                 Melody
             </Text>
-            <Link href='./8intervals'>
-                Previous: Intervals
-            </Link>
-            <Link href='./10chords'>
-                Next: Chords
-            </Link>
+            <Text style={styles.text}>
+                With the knowledge of pitch, rhythm, meter, scales, & intervals, it is now possible to create original melodies. A 
+                <b>melody</b> is a sequence of singular notes organized in a rhythmic manner, forming the main tune of a song or
+                composition. Ideally, a good melody should be catchy, singable, and easy to remember.
+            </Text>
+            <Text style={styles.header}>
+                Melodic Characteristics
+            </Text>
+            <Text style={styles.text}>
+                A melody is primarily defined by pitches, rhythms, and intervals. Melodies are created from a series of notes, each 
+                with a specific pitch & duration. These notes are arranged in a pattern of rhythm, which adds to its overall musical
+                character. 
+            </Text>
+            <Text style={styles.text}>
+                A melody can be conjunct or disjunct depending on the prominence of certain melodic intervals. <b>Conjunct melodies</b>
+                move primarily through stepwise motion. This means that conjunct melodies have mostly intervals of 2nds, keeping the notes
+                close together and creating a sense of smoothness throughout the melody. 
+            </Text>
+            <Text style={styles.text}>
+                <b>Disjunct melodies</b> move primarily through leaps, which are intervals larger than a 2nd. This keeps the notes apart 
+                and creates a sense of separation throughout the melody.
+            </Text>
+            <Text style={styles.text}>
+                Usually, a melody will consist mostly of stepwise motion and occasional leaps of a 3rd or more, which often serve as 
+                the most memorable parts of a melody. 
+            </Text>
+            <View style={styles.links}>
+                <Link href='./8intervals' style={styles.edgelinks}>
+                    Previous: Intervals
+                </Link>
+                <Link href='/(tabs)/home' style={styles.homelink}>
+                    MusicApp
+                </Link>
+                <Link href='./10chords' style={styles.edgelinks}>
+                    Next: Chords
+                </Link>
+            </View>
         </ScrollView>
     );
 }
@@ -24,8 +55,47 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         alignItems: 'center'
     },
+    title: {
+        color: 'black',
+        fontSize: 80,
+        fontFamily: 'TIMES_NEW_ROMAN',
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        paddingBottom: 20
+    },
     text: {
         color: 'black',
-        fontSize: 14
+        fontSize: 20,
+        fontFamily: 'ARIAL',
+        padding: 10
+    },
+    header: {
+        color: 'black',
+        fontSize: 36,
+        fontFamily: 'ARIAL',
+        fontWeight: 'bold',
+        padding: 40,
+        paddingTop: 60,
+        textAlign: 'left',
+    },
+    buttons: {
+        flexDirection: 'row'
+    },
+    examples: {
+        alignItems: 'flex-start'
+    },
+    links: {
+        flexDirection: 'row',
+        padding: 40,
+        gap: 500
+    },
+    edgelinks: {
+        color: 'purple',
+        fontSize: 30
+    },
+    homelink: {
+        color: 'purple',
+        fontSize: 30,
+        alignSelf: 'center'
     }
 })
