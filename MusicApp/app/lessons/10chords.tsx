c0dc45dcf0e4f9823b075786648b39ee7b1e089a
va2324 <VasuK1234@outlook.com>
2025-04-21 22:15:56 -0500

    added chords content

--- MusicApp/app/lessons/10chords.tsx
+++ MusicApp/app/lessons/10chords.tsx
@@ -40,6 +40,170 @@ export default function Chords(){
                     />
                 </View>
                 
+                <View style={styles.card}>
+                    <Text style={styles.header}>
+                        Qualities
+                    </Text>
+                    <Text style={styles.text}>
+                        There are four qualities of triads: Major, Minor, Diminished, & Augmented. These qualities are 
+                        determined by the intervals from the root to the third & from the root to the fifth. <Text style={styles.bold}>Major</Text> 
+                        & <Text style={styles.bold}>minor</Text> triads have perfect fifths and are named for the quality of their third. 
+                        A <Text style={styles.bold}>diminished</Text> triad has a minor third and a diminished fifth. An <Text style={styles.bold}>augmented</Text> 
+                        triad has a major triad and an augmented fifth. Major triads tend to sound happy. Minor triads tend to sound sad. 
+                        Diminished triads tend to sound scary. Augmented triads tend to sound mystical. 
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/triad_qualities.png')}
+                        style={styles.image}
+                        resizeMode="contain" 
+                        />
+                </View>
+
+                <View style={styles.card}>
+                    <Text style={styles.header}>
+                        Chord Symbols
+                    </Text>
+                    <Text style={styles.text}>
+                        <Text style={styles.bold}>Chord symbols</Text> for triads include the name of the root and an indication of the 
+                        chord's quality, and sometimes the pitch of the lowest note. The chord symbol begins with a capital letter
+                        denoting the name of the root (and an accidental, if necessary). This is followed by the chord's quality, 
+                        which is indicated using abbreviations. A major triad is indicated with no abbreviation at all. A minor triad 
+                        is indicated by a lowercase "m". A diminished triad is indicated by a superscript circle (o). An augmented 
+                        triad is indicated by a plus sign (+). If a triad's lowest note is any other note besides the root, then a slash
+                        is added, followed by a capital letter denoting the pitch in the bass (lowest voice).
+                    </Text>         
+                    <Image 
+                        source={require('@/assets/images/chord_symbols.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />    
+                </View>
+
+                <View style={styles.card}>
+                    <Text style={styles.header}>
+                        Triads in Scales
+                    </Text>
+                    <Text style={styles.text}>
+                        Any note in a major or minor scale can be the root of a triad. In a major scale, triads built on scale degrees
+                        1, 4, & 5 are major. Triads built on scale degrees 2, 3, & 6 are minor. Triads built on scale degree 7 are 
+                        diminished. This pattern of triads remains the same in all major scales, no matter what the key is. 
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/triads_major.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                    <Text style={styles.text}>
+                        In a minor scale, the qualities of triads can be complicated by the existence of multiple minor scales. The 
+                        triads built on scale degrees 5 & 7 can be created with or without a raised leading tone. Triads built on 
+                        scale degrees 1, 4, & 5 without a raised leading tone are minor. Triads built on scale degrees 3, 6, & 7
+                        without a raised leading tone are major. A triad built on scale degree 5 with a raised leading tone is also major.
+                        Triads built on scale degrees 2 & 7 with a raised leading tone are diminished. 
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/triads_minor.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                </View>
+
+                <View style={styles.card}>
+                    <Text style={styles.header}>
+                        Spelling & Identifying Triads
+                    </Text>
+                    <Text style={styles.text}>
+                        To build a triad from a chord symbol, it is important to be aware of the triad's root & quality. 
+                    </Text>
+                    <View style={styles.examples}>
+                        <Text style={styles.text}>
+                            1. Draw the root on the staff.
+                        </Text>
+                        <Text style={styles.text}>
+                            2. Draw the third & fifth above the root on the staff (draw a snowperson).
+                        </Text>
+                        <Text style={styles.text}>
+                            3. Remember the major key signature of the root.
+                        </Text>
+                        <Text style={styles.text}>
+                            4. To spell a major triad, write any accidentals from the key signature that apply to the notes of the triad.
+                        </Text>
+                        <Text style={styles.text}>
+                            5. For a minor, diminished, or augmented triad, add additional accidentals to alter the third or fifth when 
+                            appropriate. 
+                        </Text>
+                    </View>
+                    <Image 
+                        source={require('@/assets/images/D-triad.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                    <Text style={styles.text}>
+                        The chord's root, D, is drawn on the staff. The notes F & A are drawn as the generic third & fifth above D.
+                        The key signature of D major has been recalled. D major has two sharps: F# & C#. Because F# is in the key 
+                        signature, a sharp has been added to F in the triad. No C# is necessary because C is not present in the triad.
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/Ab_triad.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                    <Text style={styles.text}>
+                        To draw an Ab minor chord, we start by drawing the root of the chord, Ab, on the staff. The notes C & E are 
+                        drawn as the generic third & fifth above Ab. The key signature of Ab major is recalled. Ab major has 4 flats: 
+                        Bb, Eb, Ab, & Db. Eb is added because it is in the key signature of Ab. Bb & Db are not needed since they 
+                        are not in the triad. We now have an Ab major triad. To make it minor, we need to give it a minor third, which 
+                        is one half-step smaller than a major third. The last step is to lower the chord's third, C, by a half-step 
+                        to Cb. We now have an Ab minor triad.
+                    </Text>
+                    <Text style={styles.text}>
+                        Diminished triads have a minor third & diminished fifth, so both the third & the fifth have to be lowered by 
+                        a half-step from the major triad. An augmented triad has a major third & an augmented fifth, so its fifth 
+                        must be raised by a half-step from the major triad. 
+                    </Text>
+                    <Text style={styles.text}>
+                        Triads are identified according to their root & quality. First, the root must be identified. Then the major key 
+                        signature of the root must be imagined. Finally, the quality must be identified.
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/DM_triad.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                    <Text style={styles.text}>
+                        This triad is written in its most compact form, so the root is its lowest note, D. The key signature of D major has 
+                        two sharps: F# & C#. In this triad, the F is sharp, thus matching the major key signature. Therefore, this 
+                        is a D major triad. 
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/C#_triad.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                    <Text style={styles.text}>
+                        The root of this triad is its lowest note, C#. The key signature of C# major has 7 sharps (every note is sharp).
+                        Thus, E & G would be sharp in a C# major chord. However, they are both natural in the triad. Since both the 
+                        third & the fifth have been lowered by a half-step, the quality of this triad is diminished. This is a 
+                        C# diminished triad.  
+                    </Text>
+                </View>
+
+                <View style={styles.card}>
+                    <Text style={styles.header}>
+                        Doubling & Spacing
+                    </Text>
+                    <Text style={styles.text}>
+                        Because of the principle of octave equivalence, the doubling & spacing of notes in a triad does not affect the 
+                        triad's identification. Triads will remain the same regardless of octave doublings or the use of open spacing 
+                        with wide intervals. To identify triads with doublings & open spacing, one must simply imagine or write the notes 
+                        of the triad in closed spacing without any doublings. 
+                    </Text>
+                    <Image 
+                        source={require('@/assets/images/big_triads.png')}
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                </View>
+
                 <View style={styles.linksContainer}>
                     <View style={styles.linkWrapper}>
                         <Link href='./9melody' style={styles.secondaryLink}>
@@ -105,6 +269,13 @@ const styles = StyleSheet.create({
         lineHeight: 24,
         textAlign: 'left',
     },
+    header: {
+        color: '#5543A5',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        textAlign: 'left',
+    },
     bold: {
         fontWeight: 'bold',
         color: '#5543A5',
@@ -120,6 +291,9 @@ const styles = StyleSheet.create({
         height: 150,
         marginVertical: 15,
         borderRadius: 8,
+    },
+    examples: {
+        alignItems: 'flex-start'
     },
     linksContainer: {
         width: '100%',
